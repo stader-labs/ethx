f1d3d77c72349cde4f3e607634c4e577bb425476
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2022-10-03 22:28:13 +0530

    pushed timelockOwner contract

---
+++ contracts/TimelockOwner.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+abstract contract TimelockOwner is Initializable {
+    ///@notice time in secs for withholding ownership transfer
+    uint256 public lockedPeriod;
+
+    ///@dev timestamp of change owner request, owner can be changed after waiting for lockedPeriod
+    uint256 timestamp;
+
+    /// @notice address of multisig admin account which act as the owner for Stader's Ethereum Staking DAO operations
+    address timelockOwner;
+
+    ///@notice address of new multisig admin account which will act as the new owner for Stader's
+    /// Ethereum Staking DAO operations after lockedPeriod if change owner proposal not cancelled in the meantime
+    address timelockOwnerCandidate;
+
+    /// @notice event emitted when owner is updated
+    event OwnerUpdated(address indexed newOwner);
+
+    /// @notice Check for zero address
+    /// @dev Modifier
+    /// @param _address the address to check
+    modifier checkZeroAddress(address _address) {
+        require(_address != address(0), "Address cannot be zero");
+        _;
+    }
+    /// @notice Check for TimelockOwner
+    /// @dev Modifier
+    modifier checkTimelockOwner() {
+        if (msg.sender != timelockOwner)
+            revert("Errr NOT autorized for DAO operation");
+        _;
+    }
+
+    /// @notice initialize the setup parameters
+    /// @param _timelockOwner the address of owner/admin for carrying out the Doa operations
+    function initializeTimelockOwner(address _timelockOwner)
+        internal
+        onlyInitializing
+    {
+        timelockOwner = _timelockOwner;
+        lockedPeriod = 7200;
+        emit OwnerUpdated(_timelockOwner);
+    }
+
+    /**
+     * @dev Proposes a new TimelockOwner. Can only be called by the current timelockOwner
+     */
+    function proposeTimelockOwner(address _timelockOwnerCandidate)
+        external
+        checkZeroAddress(_timelockOwnerCandidate)
+        checkTimelockOwner
+    {
+        timestamp = block.timestamp;
+        timelockOwnerCandidate = _timelockOwnerCandidate;
+    }
+
+    /**
+     * @notice Assigns the ownership of the contract to timelockOwnerCandidate. Can only be called by the timelockOwner.
+     * @dev new time lock owner can be set after waiting for lockedPeriod
+     */
+    function acceptTimelockOwnership()
+        external
+        checkZeroAddress(timelockOwnerCandidate)
+        checkTimelockOwner
+    {
+        require(
+            timestamp + lockedPeriod >= block.timestamp,
+            "Locking period not expired"
+        );
+        timelockOwner = timelockOwnerCandidate;
+        emit OwnerUpdated(timelockOwnerCandidate);
+    }
+
+    /**
+     * @dev Cancels the new time lock owner proposal.
+     * Can only be called by the timelockOwnerCandidate or timelockOwner
+     */
+    function cancelTimelockOwnerProposal() external {
+        if (timelockOwnerCandidate != msg.sender && timelockOwner != msg.sender)
+            revert(
+                "ERR NOT autorized for cancelling timelock ownership proposal"
+            );
+        timelockOwnerCandidate = address(0x0);
+    }
+
+    /**********************
+     * Getter functions   *
+     **********************/
+
+    /**
+     * @dev Returns the address of the current timelock owner.
+     */
+    function getTimelockOwner() public view virtual returns (address) {
+        return timelockOwner;
+    }
+
+    /**
+     * @dev Returns the address of the new timelock owner candidate.
+     */
+    function getTimelockOwnerCandidate()
+        external
+        view
+        virtual
+        returns (address)
+    {
+        return timelockOwnerCandidate;
+    }
+
+    /**********************
+     * Setter functions   *
+     **********************/
+
+    /// @notice Set the locking period for the transfer of timelock ownership
+    /// @param _lockedPeriod time in secs for withholding timelock ownership transfer
+    function setLockedPeriod(uint256 _lockedPeriod)
+        external
+        checkTimelockOwner
+    {
+        lockedPeriod = _lockedPeriod;
+    }
+}
