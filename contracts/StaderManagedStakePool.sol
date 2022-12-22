c42003d94ec2509a0e7ec7ad4acc096e146c59bd
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2022-12-22 20:22:46 +0530

    run prettier

--- contracts/StaderManagedStakePool.sol
+++ contracts/StaderManagedStakePool.sol
@@ -97,7 +97,7 @@ contract StaderManagedStakePool is
         require(registeredValidatorCount <= validatorCount, 'not enough validator to register');
         (, bytes memory pubKey, bytes memory signature, bytes32 depositDataRoot, , , , , ) = staderValidatorRegistry
             .validatorRegistry(registeredValidatorCount);
-            
+
         //slither-disable-next-line arbitrary-send-eth
         ethValidatorDeposit.deposit{value: DEPOSIT_SIZE}(pubKey, withdrawCredential, signature, depositDataRoot);
         staderValidatorRegistry.incrementRegisteredValidatorCount();
