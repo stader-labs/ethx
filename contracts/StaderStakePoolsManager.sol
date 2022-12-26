aac6cc40d30daa48b04d2e91500e24e926808149
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2022-12-26 13:21:15 +0530

    slither fix

--- contracts/StaderStakePoolsManager.sol
+++ contracts/StaderStakePoolsManager.sol
@@ -387,13 +387,13 @@ contract StaderStakePoolsManager is IStaderStakePoolManager, TimelockControllerU
         uint256 amount = numberOfDeposits * DEPOSIT_SIZE;
         bufferedEth -= (amount);
 
-        //slither-disable-next-line low-level-calls arbitrary-send-eth
+        //slither-disable-next-line low-level-calls,arbitrary-send-eth
         (bool ssvPoolSuccess, ) = (poolParameters[0].poolAddress).call{
             value: (amount * poolParameters[0].poolWeight) / 100
         }('');
         require(ssvPoolSuccess, 'SSV Pool ETH transfer failed');
 
-        //slither-disable-next-line low-level-calls arbitrary-send-eth
+        //slither-disable-next-line low-level-calls,arbitrary-send-eth
         (bool staderPoolSuccess, ) = payable(poolParameters[1].poolAddress).call{
             value: (amount * poolParameters[1].poolWeight) / 100
         }('');
@@ -504,7 +504,7 @@ contract StaderStakePoolsManager is IStaderStakePoolManager, TimelockControllerU
         uint256 shares
     ) internal virtual {
         ethX.burnFrom(owner, shares);
-        //slither-disable-next-line low-level-calls arbitrary-send-eth
+        //slither-disable-next-line low-level-calls,arbitrary-send-eth
         (bool success, ) = payable(receiver).call{value: assets}('');
         require(success, 'withdraw failed');
         emit Withdrawn(caller, receiver, owner, assets, shares);
