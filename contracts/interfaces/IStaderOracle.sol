f999d656f1322f9eeb55094fb55fb19156ed5ceb
arigatodl <99845398+dulguun-staderlabs@users.noreply.github.com>
2023-02-17 12:14:01 +0700

    Remove protocolFee from the ER

--- contracts/interfaces/IStaderOracle.sol
+++ contracts/interfaces/IStaderOracle.sol
@@ -9,17 +9,9 @@ interface IStaderOracle {
         uint256 totalEth,
         uint256 stakingEth,
         uint256 ethxSupply,
-        uint256 protocolFee,
         uint256 time
     );
-    event BalancesUpdated(
-        uint256 block,
-        uint256 totalEth,
-        uint256 stakingEth,
-        uint256 ethxSupply,
-        uint256 protocolFee,
-        uint256 time
-    );
+    event BalancesUpdated(uint256 block, uint256 totalEth, uint256 stakingEth, uint256 ethxSupply, uint256 time);
 
     // The block number which balances are current for
     function lastBlockNumber() external view returns (uint256);
@@ -36,8 +28,6 @@ interface IStaderOracle {
     // The frequency in blocks at which network balances should be submitted by trusted nodes
     function balanceUpdateFrequency() external view returns (uint256);
 
-    function protocolFee() external view returns (uint256);
-
     function trustedNodesCount() external view returns (uint256);
 
     function isTrustedNode(address) external view returns (bool);
@@ -46,8 +36,7 @@ interface IStaderOracle {
         uint256 _block,
         uint256 _totalEth,
         uint256 _stakingEth,
-        uint256 _ethxSupply,
-        uint256 _protocolFee
+        uint256 _ethxSupply
     ) external;
 
     function getLatestReportableBlock() external view returns (uint256);
