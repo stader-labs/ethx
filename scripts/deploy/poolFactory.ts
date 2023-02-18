e19cd987956697150b07d346acc83f3010639247
arigatodl <99845398+dulguun-staderlabs@users.noreply.github.com>
2023-02-18 23:12:31 +0700

    Add poolFactory deploy script

---
+++ scripts/deploy/poolFactory.ts
@@ -0,0 +1,11 @@
+import { ethers, upgrades } from 'hardhat'
+async function main() {
+  const poolFactoryContract = await ethers.getContractFactory('PoolFactory')
+  const poolFactoryInstance = await upgrades.deployProxy(poolFactoryContract, [
+  ])
+
+  await poolFactoryInstance.deployed()
+  console.log('poolFactory deployed to:', poolFactoryInstance.address)
+}
+
+main()
