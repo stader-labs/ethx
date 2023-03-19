184b8c6bd788992525cf233a06004683e3f58067
Manoj Patra <manoj9april@gmail.com>
2023-03-19 23:38:46 +0530

    add treasury, sspm

--- contracts/StaderConfig.sol
+++ contracts/StaderConfig.sol
@@ -6,6 +6,9 @@ import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol'
 contract StaderConfig is Initializable, AccessControlUpgradeable {
     uint256 public totalStakedEth;
 
+    address public treasury;
+    address public stakePoolManager;
+
     function initialize(address _admin) external initializer {
         __AccessControl_init();
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
@@ -15,5 +18,13 @@ contract StaderConfig is Initializable, AccessControlUpgradeable {
 
     function updateTotalStakedEth(uint256 _totalStakedEth) external onlyRole(DEFAULT_ADMIN_ROLE) {
         totalStakedEth = _totalStakedEth;
+    }
+
+    function updateTreasury(address _treasury) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        treasury = _treasury;
+    }
+
+    function updateStakePoolManager(address _stakePoolManager) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        stakePoolManager = _stakePoolManager;
     }
 }
--- contracts/interfaces/IStaderConfig.sol
+++ contracts/interfaces/IStaderConfig.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.16;
 
 interface IStaderConfig {
     function totalStakedEth() external view returns (uint256);
+
+    function treasury() external view returns (address);
+
+    function stakePoolManager() external view returns (address);
 }
