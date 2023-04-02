cd55d3f21e40b8194cf317eac127234717814e3c
Dulguun <99845398+dulguun-staderlabs@users.noreply.github.com>
2023-04-02 23:10:03 +0800

    Oracle: Validator stats (#73)
    - Resolved the updateFrequency bug by correcting the order of operations for / and *.
    - Introduced a new method, submitValidatorStats, which encompasses counts and balances for each validator status.
    - For enhanced readability, gas efficiency, and maintainability, individual structs are now used for each oracle update function.
    - Removed unnecessary functions.

--- contracts/interfaces/IStaderOracle.sol
+++ contracts/interfaces/IStaderOracle.sol
@@ -24,6 +24,38 @@ struct RewardsData {
     uint256 operatorSDRewards;
 }
 
+/// @title ExchangeRate
+/// @notice This struct holds data related to the exchange rate between ETH and ETHX.
+struct ExchangeRate {
+    /// @notice The block number when the exchange rate was last updated.
+    uint256 lastUpdatedBlockNumber;
+    /// @notice The total balance of Ether (ETH) in the system.
+    uint256 totalETHBalance;
+    /// @notice The total balance of staked Ether (ETH) in the system.
+    uint256 totalStakingETHBalance;
+    /// @notice The total supply of the liquid staking token (ETHX) in the system.
+    uint256 totalETHXSupply;
+}
+
+/// @title ValidatorStats
+/// @notice This struct holds statistics related to validators in the beaconchain.
+struct ValidatorStats {
+    /// @notice The block number when the validator stats was last updated.
+    uint256 lastUpdatedBlockNumber;
+    /// @notice The total balance of all active validators.
+    uint128 activeValidatorsBalance;
+    /// @notice The total balance of all exited validators.
+    uint128 exitedValidatorsBalance;
+    /// @notice The total balance of all slashed validators.
+    uint128 slashedValidatorsBalance;
+    /// @notice The number of currently active validators.
+    uint32 activeValidatorsCount;
+    /// @notice The number of validators that have exited.
+    uint32 exitedValidatorsCount;
+    /// @notice The number of validators that have been slashed.
+    uint32 slashedValidatorsCount;
+}
+
 interface IStaderOracle {
     // Events
     event BalancesSubmitted(
@@ -37,21 +69,30 @@ interface IStaderOracle {
     event BalancesUpdated(uint256 block, uint256 totalEth, uint256 stakingEth, uint256 ethxSupply, uint256 time);
     event TrustedNodeAdded(address indexed node);
     event TrustedNodeRemoved(address indexed node);
-    event BalanceUpdateFrequencyUpdated(uint256 balanceUpdateFrequency);
     event SocializingRewardsMerkleRootSubmitted(address indexed node, uint256 index, bytes32 merkleRoot, uint256 block);
     event SocializingRewardsMerkleRootUpdated(uint256 index, bytes32 merkleRoot, uint256 block);
-
-    // The block number which balances are current for
-    function lastBlockNumberBalancesUpdated() external view returns (uint256);
-
-    // The current network total ETH balance
-    function totalETHBalance() external view returns (uint256);
-
-    // The current network staking ETH balance
-    function totalStakingETHBalance() external view returns (uint256);
-
-    // The current network total ETHX supply
-    function totalETHXSupply() external view returns (uint256);
+    event UpdateFrequencyUpdated(uint256 updateFrequency);
+    event ValidatorStatsSubmitted(
+        address indexed from,
+        uint256 block,
+        uint256 activeValidatorsBalance,
+        uint256 exitedValidatorsBalance,
+        uint256 slashedValidatorsBalance,
+        uint256 activeValidatorsCount,
+        uint256 exitedValidatorsCount,
+        uint256 slashedValidatorsCount,
+        uint256 time
+    );
+    event ValidatorStatsUpdated(
+        uint256 block,
+        uint256 activeValidatorsBalance,
+        uint256 exitedValidatorsBalance,
+        uint256 slashedValidatorsBalance,
+        uint256 activeValidatorsCount,
+        uint256 exitedValidatorsCount,
+        uint256 slashedValidatorsCount,
+        uint256 time
+    );
 
     // The root of the merkle tree containing the socializing rewards of operator
     function socializingRewardsMerkleRoot(uint256) external view returns (bytes32);
@@ -59,8 +100,12 @@ interface IStaderOracle {
     // The last updated merkle tree index
     function getCurrentRewardsIndex() external view returns (uint256);
 
-    // The frequency in blocks at which network balances should be submitted by trusted nodes
-    function balanceUpdateFrequency() external view returns (uint256);
+    function getExchangeRate() external view returns (ExchangeRate memory);
+
+    function getValidatorStats() external view returns (ValidatorStats memory);
+
+    // The frequency in blocks at which network updates should be submitted by trusted nodes
+    function updateFrequency() external view returns (uint256);
 
     function trustedNodesCount() external view returns (uint256);
 
@@ -74,17 +119,9 @@ interface IStaderOracle {
 
     /**
     @dev Submits the given balances for a specified block number.
-    @param _block The block number at which the balances are being submitted.
-    @param _totalEth The total amount of ETH in the system at the specified block number.
-    @param _stakingEth The amount of ETH currently staked in the beaconchain at the specified block number.
-    @param _ethxSupply The total supply of ETHX tokens at the specified block number.
+    @param _exchangeRate The exchange rate to submit.
     */
-    function submitBalances(
-        uint256 _block,
-        uint256 _totalEth,
-        uint256 _stakingEth,
-        uint256 _ethxSupply
-    ) external;
+    function submitBalances(ExchangeRate calldata _exchangeRate) external;
 
     /**
     @notice Submits the root of the merkle tree containing the socializing rewards.
@@ -93,6 +130,23 @@ interface IStaderOracle {
     @param _rewardsData contains rewards merkleRoot and rewards split
     */
     function submitSocializingRewardsMerkleRoot(RewardsData calldata _rewardsData) external;
+
+    /*
+     * @notice Submit validator stats for a specific block.
+     * @dev This function can only be called by trusted nodes.
+     * @param _validatorStats The validator stats to submit.
+     *
+     * Function Flow:
+     * 1. Validates that the submission is for a past block and not a future one.
+     * 2. Validates that the submission is for a block higher than the last block number with updated counts.
+     * 3. Generates submission keys using the input parameters.
+     * 4. Validates that this is not a duplicate submission from the same node.
+     * 5. Updates the submission count for the given counts.
+     * 6. Emits a ValidatorCountsSubmitted event with the submitted data.
+     * 7. If the submission count reaches a majority (trustedNodesCount / 2 + 1), checks whether the counts are not already updated,
+     *    then updates the validator counts, and emits a CountsUpdated event.
+     */
+    function submitValidatorStats(ValidatorStats calldata _validatorStats) external;
 
     function getLatestReportableBlock() external view returns (uint256);
 }
