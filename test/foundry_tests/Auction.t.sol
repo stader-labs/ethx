6b05c704111e7b2f5012e67f8efe99f95b1681d0
Manoj Patra <manoj9april@gmail.com>
2023-05-03 20:59:48 +0530

    few more tests for auction.sol

--- test/foundry_tests/Auction.t.sol
+++ test/foundry_tests/Auction.t.sol
@@ -10,14 +10,14 @@ import '../../contracts/StaderConfig.sol';
 import '../mocks/StaderTokenMock.sol';
 
 contract AuctionTest is Test {
-    uint256 testNumber;
+    address staderAdmin;
     Auction auction;
     StaderConfig staderConfig;
     StaderTokenMock staderToken;
 
     function setUp() public {
-        testNumber = 42;
-        address staderAdmin = 0x7FFCbC0b2c43d6e32A5B40FBdeA99d2d803f4Dd7;
+        staderAdmin = vm.addr(100);
+        address ethDepositAddr = vm.addr(101);
 
         staderToken = new StaderTokenMock();
         ProxyAdmin admin = new ProxyAdmin();
@@ -29,7 +29,7 @@ contract AuctionTest is Test {
             ''
         );
         staderConfig = StaderConfig(address(configProxy));
-        staderConfig.initialize(staderAdmin, vm.addr(1));
+        staderConfig.initialize(staderAdmin, ethDepositAddr);
         vm.startPrank(staderAdmin);
         staderConfig.updateStaderToken(address(staderToken));
         vm.stopPrank();
@@ -40,22 +40,103 @@ contract AuctionTest is Test {
         auction.initialize(staderAdmin, address(staderConfig));
     }
 
-    function test_NumberIs42() public {
-        assertEq(testNumber, 42);
+    function test_auctionInitialize() public {
+        assertEq(address(auction.staderConfig()), address(staderConfig));
+        assertEq(auction.duration(), auction.MIN_AUCTION_DURATION());
+        assertEq(auction.bidIncrement(), 1e16);
+        assertEq(auction.nextLot(), 1);
+        assertTrue(auction.hasRole(auction.DEFAULT_ADMIN_ROLE(), staderAdmin));
     }
 
-    function test_auctionIsCreated() public {
-        uint256 sdAmount = 2e18;
+    function testFail_insufficientSDAuctionCreate(uint256 sdAmount) public {
+        uint256 userSDBalanceBefore = staderToken.balanceOf(address(this));
+
+        vm.assume(sdAmount > userSDBalanceBefore);
+        staderToken.approve(address(auction), sdAmount);
+        auction.createLot(sdAmount);
+    }
+
+    function test_auctionIsCreated(uint256 sdAmount) public {
+        uint256 userSDBalanceBefore = staderToken.balanceOf(address(this));
+
+        vm.assume(sdAmount <= userSDBalanceBefore);
 
         assertEq(auction.nextLot(), 1);
 
         staderToken.approve(address(auction), sdAmount);
         auction.createLot(sdAmount);
 
+        assertEq(staderToken.balanceOf(address(this)), userSDBalanceBefore - sdAmount);
+
         assertEq(auction.nextLot(), 2);
+        (uint256 _startBlock, uint256 _endBlock, uint256 _sdAmount, , , , ) = auction.lots(1);
+        assertEq(_startBlock, block.number);
+        assertEq(_endBlock, block.number + auction.duration());
+        assertEq(_sdAmount, sdAmount);
+        assertEq(staderToken.balanceOf(address(auction)), sdAmount);
     }
 
-    function testFail_Subtract43() public {
-        testNumber -= 43;
+    function test_addBid(
+        uint256 sdAmount,
+        uint256 u1_bid1,
+        uint256 u1_bid2
+    ) public {
+        uint256 deployerSDBalance = staderToken.balanceOf(address(this));
+        vm.assume(sdAmount <= deployerSDBalance);
+
+        address user1 = vm.addr(1);
+        address user2 = vm.addr(2);
+
+        staderToken.approve(address(auction), sdAmount);
+        auction.createLot(sdAmount);
+
+        vm.assume(u1_bid1 < auction.bidIncrement());
+        hoax(user1, u1_bid1); // sets user1 as next method's caller and sends u1_bid1 eth
+        vm.expectRevert();
+        auction.addBid{value: u1_bid1}(1);
+
+        vm.assume(u1_bid2 > auction.bidIncrement());
+        // vm.assume(u1_bid2 + u1_bid2 < type(uint256).max )
+        hoax(user1, u1_bid2);
+        auction.addBid{value: u1_bid2}(1);
+        (, , , address highestBidder, uint256 highestBidAmount, , ) = auction.lots(1);
+        assertEq(highestBidder, user1);
+        assertEq(highestBidAmount, u1_bid2);
+    }
+
+    // used uint128 else it will overflow
+    function test_addBidByAnotherUser(
+        uint256 sdAmount,
+        uint128 u1_bid1,
+        uint256 u2_bid1,
+        uint256 u2_bid2
+    ) public {
+        uint256 deployerSDBalance = staderToken.balanceOf(address(this));
+        vm.assume(sdAmount <= deployerSDBalance);
+
+        address user1 = vm.addr(1);
+        address user2 = vm.addr(2);
+
+        staderToken.approve(address(auction), sdAmount);
+        auction.createLot(sdAmount);
+
+        vm.assume(u1_bid1 > auction.bidIncrement());
+        hoax(user1, u1_bid1);
+        auction.addBid{value: u1_bid1}(1);
+        (, , , , uint256 highestBidAmount, , ) = auction.lots(1);
+
+        // used uint128, else [highestBidAmount + auction.bidIncrement()] will overflow
+        vm.assume(u2_bid1 < highestBidAmount + auction.bidIncrement());
+        hoax(user2, u2_bid1);
+        vm.expectRevert();
+        auction.addBid{value: u2_bid1}(1);
+
+        // commented below as it is failing
+        // vm.assume(u2_bid2 >= highestBidAmount + auction.bidIncrement());
+        // hoax(user2, u2_bid2);
+        // auction.addBid{value: u2_bid2}(1);
+        // // (, , , address highestBidder, uint256 highestBidAmount2, , ) = auction.lots(1);
+        // // assertEq(highestBidder, user2);
+        // // assertEq(highestBidAmount2, u2_bid2);
     }
 }
