45f225cc43e5f8eb39f88c5347893554430fe0f5
Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>
2023-05-09 20:10:02 +0530

    permissionless node registry test

--- test/foundry_tests/PermissionlessNodeRegistry.t.sol
+++ test/foundry_tests/PermissionlessNodeRegistry.t.sol
@@ -9,7 +9,9 @@ import '../../contracts/PermissionlessNodeRegistry.sol';
 import '../mocks/PoolUtilsMock.sol';
 import '../mocks/SocializingPoolMock.sol';
 import '../mocks/SDCollateralMock.sol';
+import '../mocks/StaderOracleMock.sol';
 import '../mocks/PermissionlessPoolMock.sol';
+import '../mocks/StaderInsuranceFundMock.sol';
 
 import 'forge-std/Test.sol';
 import '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol';
@@ -18,21 +20,26 @@ import '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
 contract PermissionlessNodeRegistryTest is Test {
     address staderAdmin;
     address staderManager;
+    address operator;
 
     StaderConfig staderConfig;
     VaultFactory vaultFactory;
     PermissionlessNodeRegistry nodeRegistry;
 
     SocializingPoolMock socializingPoolMock;
+    StaderOracleMock staderOracle;
 
     function setUp() public {
         staderAdmin = vm.addr(100);
         staderManager = vm.addr(101);
-        address ethDepositAddr = vm.addr(102);
+        operator = vm.addr(102);
+        address ethDepositAddr = vm.addr(103);
 
         PoolUtilsMock poolUtils = new PoolUtilsMock();
         socializingPoolMock = new SocializingPoolMock();
+        staderOracle = new StaderOracleMock();
         SDCollateralMock sdCollateral = new SDCollateralMock();
+        StaderInsuranceFundMock insuranceFund = new StaderInsuranceFundMock();
         PermissionlessPoolMock permissionlessPool = new PermissionlessPoolMock();
         ProxyAdmin admin = new ProxyAdmin();
 
@@ -64,9 +71,12 @@ contract PermissionlessNodeRegistryTest is Test {
         staderConfig.updatePoolUtils(address(poolUtils));
         staderConfig.updateVaultFactory(address(vaultFactory));
         staderConfig.updateSDCollateral(address(sdCollateral));
+        staderConfig.updateStaderOracle(address(staderOracle));
+        staderConfig.updateStaderInsuranceFund(address(insuranceFund));
         staderConfig.updatePermissionlessPool(address(permissionlessPool));
         staderConfig.updatePermissionlessSocializingPool(address(socializingPoolMock));
         staderConfig.grantRole(staderConfig.MANAGER(), staderManager);
+        staderConfig.grantRole(staderConfig.OPERATOR(), operator);
         vaultFactory.grantRole(vaultFactory.NODE_REGISTRY_CONTRACT(), address(nodeRegistry));
         vm.stopPrank();
     }
@@ -130,17 +140,79 @@ contract PermissionlessNodeRegistryTest is Test {
         assertEq(output, nodeRegistry.nodeELRewardVaultByOperatorId(operatorId));
     }
 
-    function test_addValidatorKeys(bytes calldata _pubkey, bytes calldata _signature) public {
-        vm.assume(_pubkey.length == 48);
-        vm.assume(_signature.length == 96);
+    function test_addValidatorKeys() public {
         bytes[] memory pubkeys = new bytes[](1);
         bytes[] memory preDepositSignature = new bytes[](1);
         bytes[] memory depositSignature = new bytes[](1);
-        pubkeys[0] = _pubkey;
-        preDepositSignature[0] = _signature;
-        depositSignature[0] = _signature;
+        pubkeys[0] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde336750';
+        preDepositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
+        depositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
         startHoax(address(this));
         nodeRegistry.onboardNodeOperator(true, 'testOP', payable(address(this)));
         nodeRegistry.addValidatorKeys{value: 4 ether}(pubkeys, preDepositSignature, depositSignature);
+        vm.stopPrank();
+        uint256 nextValidatorId = nodeRegistry.nextValidatorId();
+        assertEq(nextValidatorId, 2);
+        assertEq(nodeRegistry.validatorIdByPubkey(pubkeys[0]), nextValidatorId - 1);
+    }
+
+    function test_markReadyToDepositValidator() public {
+        bytes[] memory pubkeys = new bytes[](3);
+        bytes[] memory preDepositSignature = new bytes[](3);
+        bytes[] memory depositSignature = new bytes[](3);
+        pubkeys[0] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde336750';
+        pubkeys[1] = '0xa119a476cd0f30f5117b823c5732c66199136f18e55e6a';
+        pubkeys[2] = '0x8c6c13d3cc575bd0e679481d6a730ee19e73d69183518a';
+        preDepositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
+        preDepositSignature[
+            1
+        ] = '0xa119a476cd0f30f5117b823c5732c66199136f18e55e6a616a52a69f1986d8b08055bccbf7169baf47289fa2849958';
+        preDepositSignature[
+            2
+        ] = '0x8c6c13d3cc575bd0e679481d6a730ee19e73d6918351b2d42eb77ff690664348a5060ccb8938df9cedfd2998d7278b';
+        depositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
+        depositSignature[
+            1
+        ] = '0xa119a476cd0f30f5117b823c5732c66199136f18e55e6a616a52a69f1986d8b08055bccbf7169baf47289fa2849958';
+        depositSignature[
+            2
+        ] = '0x8c6c13d3cc575bd0e679481d6a730ee19e73d6918351b2d42eb77ff690664348a5060ccb8938df9cedfd2998d7278b';
+        startHoax(operator);
+        nodeRegistry.onboardNodeOperator(true, 'testOP', payable(address(this)));
+        nodeRegistry.addValidatorKeys{value: 12 ether}(pubkeys, preDepositSignature, depositSignature);
+        bytes[] memory readyToDepositKeys = new bytes[](1);
+        bytes[] memory frontRunKeys = new bytes[](1);
+        bytes[] memory invalidSigKeys = new bytes[](1);
+        readyToDepositKeys[0] = pubkeys[0];
+        frontRunKeys[0] = pubkeys[1];
+        invalidSigKeys[0] = pubkeys[2];
+        nodeRegistry.markValidatorReadyToDeposit(readyToDepositKeys, frontRunKeys, invalidSigKeys);
+    }
+
+    function test_withdrawnValidators() public {
+        bytes[] memory pubkeys = new bytes[](1);
+        bytes[] memory preDepositSignature = new bytes[](1);
+        bytes[] memory depositSignature = new bytes[](1);
+        pubkeys[0] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde336750';
+        preDepositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
+        depositSignature[
+            0
+        ] = '0x8faa339ba46c649885ea0fc9c34d32f9d99c5bde3367500ee111075fc390fa48d8dbe155633ad489ee5866e152a5f5';
+        startHoax(address(this));
+        nodeRegistry.onboardNodeOperator(true, 'testOP', payable(address(this)));
+        nodeRegistry.addValidatorKeys{value: 4 ether}(pubkeys, preDepositSignature, depositSignature);
+        vm.stopPrank();
+        vm.prank(address(staderOracle));
+        nodeRegistry.withdrawnValidators(pubkeys);
     }
 }
