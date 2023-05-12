1cfd461326128927e8d373c4c853535dc47874af
Manoj Patra <manoj9april@gmail.com>
2023-05-12 19:55:29 +0530

    penalty unit test complete

---
+++ test/mocks/PermissionedPoolMock.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.16;
+
+import '../../contracts/StaderConfig.sol';
+
+contract PermissionedPoolMock {
+    StaderConfig staderConfig;
+
+    constructor(address _staderConfigAddr) {
+        staderConfig = StaderConfig(_staderConfigAddr);
+    }
+
+    function receiveInsuranceFund() external payable {
+        UtilLib.onlyStaderContract(msg.sender, staderConfig, staderConfig.STADER_INSURANCE_FUND());
+    }
+}
