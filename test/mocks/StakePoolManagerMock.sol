6b0564504b6100a75a5ba394aac7c561c09eadca
Sanjay Yadav <104342377+sanjay-staderlabs@users.noreply.github.com>
2023-05-17 16:42:48 +0530

    Permissionless Node registry test (#141)
    * foundry initial setup
    
    * forge install: openzeppelin-contracts-upgradeable
    
    v4.8.3
    
    * wip
    
    * moved legacy hardhat test to a folder
    
    * first foundry unit test
    
    * few more tests for auction.sol
    
    * one more test
    
    * auction: test claim SD
    
    * test transferHighestBidToSSPM
    
    * test extractNonBidSD
    
    * auction test complete
    
    * some SDCollateral units tests
    
    * minor refactor: sdCollateral
    
    * fuzzy tests: sdCollateral::requestWithdraw
    
    * fuzzyTest | sdCollateral::claimWithdraw
    
    * test : SDCollateral::slashValidator
    
    * intial test of permissionless node registry
    
    * add validator tests
    
    * phase2 pull
    
    * permissionless node registry test
    
    * withdrawnValidator test
    
    * formatting changes
    
    * permissionless node registry test
    
    ---------
    
    Co-authored-by: Manoj Patra <manoj9april@gmail.com>
    Co-authored-by: Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>

--- test/mocks/StakePoolManagerMock.sol
+++ test/mocks/StakePoolManagerMock.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.16;
 contract StakePoolManagerMock {
     function receiveEthFromAuction() external payable {}
 
+    function receiveWithdrawVaultUserShare() external payable {}
+
     function receiveExecutionLayerRewards() external payable {}
 }
