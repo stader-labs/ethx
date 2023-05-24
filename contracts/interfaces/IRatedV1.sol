78f416c5a9cf74eb9aac5bde1c979c32f5d3f65d
Manoj Patra <manoj9april@gmail.com>
2023-05-24 09:36:16 +0530

    HAL-11 | fixed pragma solidity

--- contracts/interfaces/IRatedV1.sol
+++ contracts/interfaces/IRatedV1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.16;
+pragma solidity 0.8.16;
 
 interface IRatedV1 {
     function getViolationsForValidator(bytes32 _pubKeyRoot) external returns (uint256[] memory violatedEpoch);
