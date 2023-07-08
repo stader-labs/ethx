9fc819388373ed711ae9c9da1a73d32e17e82c18
Sanjay Yadav <104342377+sanjay-staderlabs@users.noreply.github.com>
2023-07-08 13:34:18 +0530

    integration guide and license (#197)
    * integration guide and license
    
    * add license to test file
    
    * review changes
    
    * formatting fix
    
    ---------
    
    Co-authored-by: Sanjay Yadav <sanjayyadav@Sanjays-MacBook-Pro.local>

---
+++ INTEGRATION.md
@@ -0,0 +1,185 @@
+
+# ETHx Integration guide
+
+  
+
+Deployment addresses can be found at:
+
+- Mainnet: [ethX-mainnet-contract](https://staderlabs.gitbook.io/ethereum/smart-contracts#ethx-mainnet-smart-contracts)
+
+- Testnet: [ethX-testnet-contract](https://staderlabs.gitbook.io/ethereum/smart-contracts#ethx-testnet-smart-contracts)
+
+  
+
+
+Stader’s ETHx is an ERC-20 token that represents staked ETH. Following are the different actions a user can take to mint/burn ETHx.
+
+
+### 1. Stake ETH
+
+Send ETH and receive minted ETHx token.
+ 
+```SOLIDITY
+
+IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+// function call staderStakePoolManager deposit function, depositing ETH and minting
+// `amountInETHx` ETHx, sent to `_receiver` address
+uint256 amountInETHx = IStaderStakePoolManager(staderConfig.getStakePoolManager()).deposit{value: _amountInETH}(
+	_receiver
+);
+
+emit  Deposited(msg.sender, _receiver, _amountInETH, amountInETHx);
+```
+
+### 2. Approve ETHx
+
+Set `_amountInETHx` as the allowance for userWithdrawalManager as a spender. This can be done at the ETHx ERC20 token contract. Alternatively, a max approval can be performed once, and this step can be skipped after the first time.
+
+```SOLIDITY
+
+IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+bool success = ETHx(staderConfig.getETHxToken()).approve(staderConfig.getUserWithdrawManager(), _amountInETHx);
+
+require(success, 'Approved Failed');
+```
+
+### 3. Unstake ETH 
+
+Create a withdrawal request to initiate ETH redemption.
+
+```SOLIDITY
+
+IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+// get userWithdrawalManager contract from staderConfig
+IUserWithdrawalManager userWithdrawalManager = IUserWithdrawalManager(staderConfig.getUserWithdrawManager());
+
+// a call to userWithdrawalManager contract's requestWithdraw function generates 
+// a unique requestID, and sets the owner of unstake request to `_owner`
+uint256 requestID = userWithdrawalManager.requestWithdraw(_amountInETHx, _owner);
+
+emit  WithdrawRequestReceived(msg.sender, _owner, requestID, _amountInETHx);
+```
+
+
+### 4. Claim ETH
+
+Call the claim function to receive the ETH to the receiver address after the withdrawRequest is finalized. This step marks the end of ETH redemption.
+
+
+```SOLIDITY
+
+IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+// get userWithdrawalManager contract from staderConfig
+IUserWithdrawalManager userWithdrawalManager = IUserWithdrawalManager(staderConfig.getUserWithdrawManager());
+
+// pass requestId from Step 3. ethFinalized is the ETH amount redeemable through a claim
+(, , , uint256 ethFinalized, ) = userWithdrawalManager.userWithdrawRequests(_requestId);
+
+// call stader userWithdrawManager contract to claim from _owner address in step 3 to receive ETH.IUserWithdrawalManager(staderConfig.getUserWithdrawManager()).claim(_requestId);
+
+emit  RequestRedeemed(msg.sender, ethFinalized);
+```
+
+
+
+### Sample Example of Stake, Unstake, and Claim
+
+  
+
+
+```SOLIDITY
+pragma  solidity 0.8.16;
+
+import  './ETHx.sol';
+import  './interfaces/IStaderConfig.sol';
+import  './interfaces/IStaderStakePoolManager.sol';
+import  './interfaces/IUserWithdrawalManager.sol';
+
+contract  Example {
+
+	event  Deposited(address  indexed  caller, address  indexed  receiver, uint256  assets, uint256  shares);
+
+	event  WithdrawRequestReceived(address  indexed  msgSender, address  owner, uint256  requestId, uint256  sharesAmount);
+
+	event  RequestRedeemed(address  receiver, uint256  ethTransferred);
+
+	address  private  STADER_CONFIG_ADDRESS = '0x4ABEF2263d5A5ED582FC9A9789a41D85b68d69DB'; //mainnet address
+
+	/**
+	 * @notice function to stake ETH and mint ETHx by interacting with StaderStakePoolManager Contract
+	 * @dev contract should have minimum ETH equal to `_amountInETH` to call this function
+	 * @param  _amountInETH amount of ETH to stake
+	 * @param  _receiver address to receive minted ETHx
+	*/
+	function  stake(uint256  _amountInETH, address  _receiver) external  payable {
+		IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+		// function call staderStakePoolManager deposit function, depositing ETH and minting
+		// `amountInETHx` ETHx, sent to `_receiver` address
+		uint256 amountInETHx = IStaderStakePoolManager(staderConfig.getStakePoolManager()).deposit{value: _amountInETH}(
+			_receiver
+		);
+
+		emit  Deposited(msg.sender, _receiver, _amountInETH, amountInETHx);
+	}
+
+	/**
+	 * @notice set `_amountInETHx` as the allowance for userWithdrawalManager as a spender
+	 * @param  _amountInETHx amount of ETHx to set as allowance
+	*/
+	function  approve(uint256  _amountInETHx) external {
+		IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+		bool success = ETHx(staderConfig.getETHxToken()).approve(staderConfig.getUserWithdrawManager(), _amountInETHx);
+		require(success, 'Approved Failed');
+	}
+
+	/**
+	 * @notice function to create unstake request by sending ETHx
+	 * @dev ensure that the contract has minimum `_amountInETHx` amount of ETHx and
+	 * ETHx approval should be given prior to this step
+	 * @param  _amountInETHx amount of ETHx to unstake
+	 * @param _owner address to be set as owner of unstake request, only owner allowed to claim
+	*/
+	function  unstake(uint256  _amountInETHx, address  _owner) external {
+		IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+		// get userWithdrawalManager contract from staderConfig
+		IUserWithdrawalManager userWithdrawalManager = IUserWithdrawalManager(staderConfig.getUserWithdrawManager());
+
+		// a call to userWithdrawalManager contract's requestWithdraw function generates 
+		// a unique requestID, and sets the owner of unstake request to `_owner`
+		uint256 requestID = userWithdrawalManager.requestWithdraw(_amountInETHx, _owner);
+
+		emit  WithdrawRequestReceived(msg.sender, _owner, requestID, _amountInETHx);
+	}
+
+	/**
+	 * @notice claim the ETH associated with a finalized unstake request
+	 * @dev call the claim function to receive the ETH to the receiver address after the 
+	 * withdrawRequest is finalized
+	 * @param  _requestId Request ID to claim
+	*/
+	function  claim(uint256  _requestId) external {
+		IStaderConfig staderConfig = IStaderConfig(STADER_CONFIG_ADDRESS);
+
+		// get userWithdrawalManager contract from staderConfig
+		IUserWithdrawalManager userWithdrawalManager = IUserWithdrawalManager(staderConfig.getUserWithdrawManager());
+
+		// ethFinalized is the ETH amount redeemable through a claim
+		(, , , uint256 ethFinalized, ) = userWithdrawalManager.userWithdrawRequests(_requestId);
+
+		//calls stader userWithdrawManager contract to claim and receives ETH
+		IUserWithdrawalManager(staderConfig.getUserWithdrawManager()).claim(_requestId);
+
+		emit  RequestRedeemed(msg.sender, ethFinalized);
+	}
+
+}
+```
+
+  
+
--- test/foundry_tests/StaderStakePoolManager.t.sol
+++ test/foundry_tests/StaderStakePoolManager.t.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.16;
 
 import '../../contracts/ETHx.sol';
@@ -14,7 +15,6 @@ import '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.so
 import '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
 
 contract StaderStakePoolManagerTest is Test {
-
     using Math for uint256;
     address staderAdmin;
     address staderManager;
@@ -23,7 +23,7 @@ contract StaderStakePoolManagerTest is Test {
     ETHx ethX;
     StaderConfig staderConfig;
     StaderStakePoolsManager stakePoolManager;
-    
+
     PoolMock permissionlessPoolAddress;
     PoolMock permissionedPoolAddress;
     PoolUtilsMock poolUtils;
@@ -122,33 +122,34 @@ contract StaderStakePoolManagerTest is Test {
     }
 
     function test_receiveExecutionLayerRewards(uint64 privateKey, uint64 amount) public {
-        vm.assume(privateKey >0);
+        vm.assume(privateKey > 0);
         address randomAddr = vm.addr(privateKey);
         startHoax(randomAddr, amount);
         assertEq(address(stakePoolManager).balance, 0);
         stakePoolManager.receiveExecutionLayerRewards{value: amount}();
         assertEq(address(stakePoolManager).balance, amount);
     }
+
     function test_receiveWithdrawVaultUserShare(uint64 privateKey, uint64 amount) public {
-        vm.assume(privateKey >0);
+        vm.assume(privateKey > 0);
         address randomAddr = vm.addr(privateKey);
         startHoax(randomAddr, amount);
         assertEq(address(stakePoolManager).balance, 0);
         stakePoolManager.receiveWithdrawVaultUserShare{value: amount}();
         assertEq(address(stakePoolManager).balance, amount);
-    }    
+    }
 
     function test_receiveEthFromAuction(uint64 privateKey, uint64 amount) public {
-        vm.assume(privateKey >0);
+        vm.assume(privateKey > 0);
         address randomAddr = vm.addr(privateKey);
         startHoax(randomAddr, amount);
         assertEq(address(stakePoolManager).balance, 0);
         stakePoolManager.receiveEthFromAuction{value: amount}();
         assertEq(address(stakePoolManager).balance, amount);
-    }  
+    }
 
     function test_receiveExcessEthFromPool(uint64 privateKey, uint64 amount) public {
-        vm.assume(privateKey >0);
+        vm.assume(privateKey > 0);
         address randomAddr = vm.addr(privateKey);
         startHoax(randomAddr, amount);
         assertEq(address(stakePoolManager).balance, 0);
@@ -165,7 +166,7 @@ contract StaderStakePoolManagerTest is Test {
     }
 
     function test_updateExcessETHDepositCoolDown(uint64 excessETHDepositCoolDown) public {
-        vm.expectRevert(UtilLib.CallerNotManager.selector);        
+        vm.expectRevert(UtilLib.CallerNotManager.selector);
         stakePoolManager.updateExcessETHDepositCoolDown(excessETHDepositCoolDown);
         vm.prank(staderManager);
         stakePoolManager.updateExcessETHDepositCoolDown(excessETHDepositCoolDown);
@@ -192,13 +193,13 @@ contract StaderStakePoolManagerTest is Test {
     function test_getExchangeRate(uint256 _totalETHx, uint256 _totalETH) public {
         vm.assume(_totalETHx > 0 && _totalETHx < 120 * 10**24);
         vm.assume(_totalETH > 0 && _totalETH < 120 * 10**24);
-        assertEq(stakePoolManager.getExchangeRate(), 1*10**18);
+        assertEq(stakePoolManager.getExchangeRate(), 1 * 10**18);
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
             abi.encode(block.number, _totalETH, _totalETHx)
         );
-        uint256 exchangeRate = (_totalETH * 10**18)/_totalETHx;
+        uint256 exchangeRate = (_totalETH * 10**18) / _totalETHx;
         assertEq(stakePoolManager.getExchangeRate(), exchangeRate);
     }
 
@@ -209,67 +210,67 @@ contract StaderStakePoolManagerTest is Test {
             abi.encode(block.number, 0, 1 ether)
         );
         vm.expectRevert();
-        stakePoolManager.convertToShares(1 ether); 
+        stakePoolManager.convertToShares(1 ether);
         vm.expectRevert();
-        stakePoolManager.previewDeposit(1 ether); 
+        stakePoolManager.previewDeposit(1 ether);
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
-            abi.encode(block.number, 1 ether,  0)
-        );  
-        assertEq(stakePoolManager.convertToShares(1 ether),1 ether); 
-        assertEq(stakePoolManager.previewDeposit(1 ether),1 ether); 
+            abi.encode(block.number, 1 ether, 0)
+        );
+        assertEq(stakePoolManager.convertToShares(1 ether), 1 ether);
+        assertEq(stakePoolManager.previewDeposit(1 ether), 1 ether);
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
             abi.encode(block.number, 1 ether, 1 ether)
         );
-        assertEq(stakePoolManager.convertToShares(1 ether),1 ether);
-        assertEq(stakePoolManager.convertToShares(0),0);
-        assertEq(stakePoolManager.previewDeposit(1 ether),1 ether);
-        assertEq(stakePoolManager.previewDeposit(0),0);
+        assertEq(stakePoolManager.convertToShares(1 ether), 1 ether);
+        assertEq(stakePoolManager.convertToShares(0), 0);
+        assertEq(stakePoolManager.previewDeposit(1 ether), 1 ether);
+        assertEq(stakePoolManager.previewDeposit(0), 0);
     }
 
     function test_convertToAssets() public {
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
-            abi.encode(block.number, 1 ether,  0)
+            abi.encode(block.number, 1 ether, 0)
         );
-        assertEq(stakePoolManager.convertToAssets(1 ether),1 ether); 
-        assertEq(stakePoolManager.convertToAssets(1000 ether),1000 ether); 
+        assertEq(stakePoolManager.convertToAssets(1 ether), 1 ether);
+        assertEq(stakePoolManager.convertToAssets(1000 ether), 1000 ether);
 
-        assertEq(stakePoolManager.previewWithdraw(1 ether),1 ether); 
-        assertEq(stakePoolManager.previewWithdraw(1000 ether),1000 ether); 
-
-        vm.mockCall(
-            address(staderOracle),
-            abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
-            abi.encode(block.number, 1000 ether,  1 ether)
-        );  
-        assertEq(stakePoolManager.convertToAssets(1 ether),1000 ether); 
-        assertEq(stakePoolManager.previewWithdraw(1 ether),1000 ether); 
+        assertEq(stakePoolManager.previewWithdraw(1 ether), 1 ether);
+        assertEq(stakePoolManager.previewWithdraw(1000 ether), 1000 ether);
 
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
-            abi.encode(block.number, 1000 ether,  500 ether)
-        );  
-        assertEq(stakePoolManager.convertToAssets(1 ether),2 ether); 
-        assertEq(stakePoolManager.previewWithdraw(1 ether),2 ether); 
+            abi.encode(block.number, 1000 ether, 1 ether)
+        );
+        assertEq(stakePoolManager.convertToAssets(1 ether), 1000 ether);
+        assertEq(stakePoolManager.previewWithdraw(1 ether), 1000 ether);
+
+        vm.mockCall(
+            address(staderOracle),
+            abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
+            abi.encode(block.number, 1000 ether, 500 ether)
+        );
+        assertEq(stakePoolManager.convertToAssets(1 ether), 2 ether);
+        assertEq(stakePoolManager.previewWithdraw(1 ether), 2 ether);
 
         vm.mockCall(
             address(staderOracle),
             abi.encodeWithSelector(IStaderOracle.getExchangeRate.selector),
             abi.encode(block.number, 0, 100 ether)
         );
-        assertEq(stakePoolManager.convertToAssets(1 ether),0);
-        assertEq(stakePoolManager.convertToAssets(1000 ether),0);
-        assertEq(stakePoolManager.convertToAssets(0),0);
+        assertEq(stakePoolManager.convertToAssets(1 ether), 0);
+        assertEq(stakePoolManager.convertToAssets(1000 ether), 0);
+        assertEq(stakePoolManager.convertToAssets(0), 0);
 
-        assertEq(stakePoolManager.previewWithdraw(1 ether),0);
-        assertEq(stakePoolManager.previewWithdraw(1000 ether),0);
-        assertEq(stakePoolManager.previewWithdraw(0),0);
+        assertEq(stakePoolManager.previewWithdraw(1 ether), 0);
+        assertEq(stakePoolManager.previewWithdraw(1000 ether), 0);
+        assertEq(stakePoolManager.previewWithdraw(0), 0);
     }
 
     function test_deposit() public {
@@ -284,7 +285,7 @@ contract StaderStakePoolManagerTest is Test {
 
         vm.expectRevert(IStaderStakePoolManager.InvalidDepositAmount.selector);
         stakePoolManager.deposit{value: 1}(receiver);
-        
+
         vm.expectRevert(IStaderStakePoolManager.InvalidDepositAmount.selector);
         stakePoolManager.deposit{value: 100000 ether}(receiver);
         assertEq(stakePoolManager.deposit{value: 100 ether}(receiver), 100 ether);
@@ -302,7 +303,7 @@ contract StaderStakePoolManagerTest is Test {
             abi.encode(block.number, 0, 1 ether)
         );
         assertEq(stakePoolManager.minDeposit(), 0);
-        assertEq(stakePoolManager.maxDeposit(), 0); 
+        assertEq(stakePoolManager.maxDeposit(), 0);
     }
 
     function test_validatorBatchDeposit() public {
@@ -318,20 +319,16 @@ contract StaderStakePoolManagerTest is Test {
             abi.encodeWithSelector(IPoolUtils.isExistingPoolId.selector),
             abi.encode(false)
         );
-        vm.expectRevert(IStaderStakePoolManager.PoolIdDoesNotExit.selector);   
+        vm.expectRevert(IStaderStakePoolManager.PoolIdDoesNotExit.selector);
         stakePoolManager.validatorBatchDeposit(1);
         stakePoolManager.deposit{value: 100 ether}(address(this));
-        vm.mockCall(
-            address(poolUtils),
-            abi.encodeWithSelector(IPoolUtils.isExistingPoolId.selector),
-            abi.encode(true)
-        );
+        vm.mockCall(address(poolUtils), abi.encodeWithSelector(IPoolUtils.isExistingPoolId.selector), abi.encode(true));
         vm.mockCall(
             address(userWithdrawManager),
             abi.encodeWithSelector(IUserWithdrawalManager.ethRequestedForWithdraw.selector),
             abi.encode(80 ether)
         );
-        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);   
+        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);
         stakePoolManager.validatorBatchDeposit(1);
 
         vm.mockCall(
@@ -339,9 +336,9 @@ contract StaderStakePoolManagerTest is Test {
             abi.encodeWithSelector(IUserWithdrawalManager.ethRequestedForWithdraw.selector),
             abi.encode(70 ether)
         );
-        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);   
-        stakePoolManager.validatorBatchDeposit(2);   
-        
+        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);
+        stakePoolManager.validatorBatchDeposit(2);
+
         vm.mockCall(
             address(poolSelector),
             abi.encodeWithSelector(IPoolSelector.computePoolAllocationForDeposit.selector),
@@ -352,21 +349,20 @@ contract StaderStakePoolManagerTest is Test {
             abi.encodeWithSelector(IUserWithdrawalManager.ethRequestedForWithdraw.selector),
             abi.encode(0)
         );
-        stakePoolManager.validatorBatchDeposit(2);  
-        assertEq(address(stakePoolManager).balance, 100 ether); 
+        stakePoolManager.validatorBatchDeposit(2);
+        assertEq(address(stakePoolManager).balance, 100 ether);
         vm.mockCall(
             address(poolSelector),
             abi.encodeWithSelector(IPoolSelector.computePoolAllocationForDeposit.selector),
             abi.encode(3)
         );
 
-        stakePoolManager.validatorBatchDeposit(2); 
-        assertEq(address(stakePoolManager).balance, 4 ether); 
-        assertEq(address(permissionedPoolAddress).balance, 96 ether); 
+        stakePoolManager.validatorBatchDeposit(2);
+        assertEq(address(stakePoolManager).balance, 4 ether);
+        assertEq(address(permissionedPoolAddress).balance, 96 ether);
     }
 
     function test_depositETHOverTargetWeight() public {
-
         stakePoolManager.deposit{value: 100 ether}(address(this));
         vm.expectRevert(IStaderStakePoolManager.CooldownNotComplete.selector);
         stakePoolManager.depositETHOverTargetWeight();
@@ -377,8 +373,8 @@ contract StaderStakePoolManagerTest is Test {
             abi.encode(70 ether)
         );
         vm.roll(block.number + stakePoolManager.excessETHDepositCoolDown());
-        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);   
-        stakePoolManager.depositETHOverTargetWeight();   
+        vm.expectRevert(IStaderStakePoolManager.InsufficientBalance.selector);
+        stakePoolManager.depositETHOverTargetWeight();
         vm.mockCall(
             address(userWithdrawManager),
             abi.encodeWithSelector(IUserWithdrawalManager.ethRequestedForWithdraw.selector),
@@ -387,20 +383,18 @@ contract StaderStakePoolManagerTest is Test {
         uint256[] memory selectedPoolCapacity = new uint256[](2);
         selectedPoolCapacity[0] = 2;
         selectedPoolCapacity[1] = 1;
-        
+
         uint8[] memory poolIdArray = new uint8[](2);
         poolIdArray[0] = 1;
         poolIdArray[1] = 2;
         vm.mockCall(
             poolSelector,
             abi.encodeWithSelector(IPoolSelector.poolAllocationForExcessETHDeposit.selector),
-            abi.encode(selectedPoolCapacity,poolIdArray)
+            abi.encode(selectedPoolCapacity, poolIdArray)
         );
-        stakePoolManager.depositETHOverTargetWeight();   
-        assertEq(address(stakePoolManager).balance, 12 ether); 
-        assertEq(address(permissionedPoolAddress).balance, 32 ether); 
+        stakePoolManager.depositETHOverTargetWeight();
+        assertEq(address(stakePoolManager).balance, 12 ether);
+        assertEq(address(permissionedPoolAddress).balance, 32 ether);
         assertEq(address(permissionlessPoolAddress).balance, 56 ether);
     }
-
 }
-
