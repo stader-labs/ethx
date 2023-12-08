2427c9fcdd7fa58030ca7d8f22a6c7b8e6fa3105
arigatodl <99845398+dulguun-staderlabs@users.noreply.github.com>
2023-12-08 18:19:57 +0800

    Add non-existent liquidation check

--- contracts/SDUtilityPool.sol
+++ contracts/SDUtilityPool.sol
@@ -441,6 +441,7 @@ contract SDUtilityPool is ISDUtilityPool, AccessControlUpgradeable, PausableUpgr
 
     function repayLiquidation(address account) external override {
         UtilLib.onlyStaderContract(msg.sender, staderConfig, staderConfig.OPERATOR_REWARD_COLLECTOR());
+        if (liquidationIndexByOperator[account] == 0) revert InvalidInput();
 
         liquidations[liquidationIndexByOperator[account] - 1].isRepaid = true;
         liquidationIndexByOperator[account] = 0;
@@ -717,7 +718,9 @@ contract SDUtilityPool is ISDUtilityPool, AccessControlUpgradeable, PausableUpgr
                 totalInterestSD,
                 totalCollateralInSD,
                 healthFactor,
-                liquidations[liquidationIndexByOperator[account] - 1].totalAmountInEth
+                liquidationIndexByOperator[account] == 0
+                    ? 0
+                    : liquidations[liquidationIndexByOperator[account] - 1].totalAmountInEth
             );
     }
 
@@ -735,6 +738,7 @@ contract SDUtilityPool is ISDUtilityPool, AccessControlUpgradeable, PausableUpgr
     }
 
     function getOperatorLiquidation(address account) external view override returns (OperatorLiquidation memory) {
+        if (liquidationIndexByOperator[account] == 0) return OperatorLiquidation(0, 0, 0, false, false, address(0));
         return liquidations[liquidationIndexByOperator[account] - 1];
     }
 
